281aa4dda515e15154ac66ce85b1a38b619b5cb7
hejzbh <hazimba123@gmail.com>
2024-10-20 14:25:47 +0200

    Eslint fix

--- components/ui/animated/FloatCard.tsx
+++ components/ui/animated/FloatCard.tsx
@@ -34,12 +34,12 @@ export const CardContainer = ({
     containerRef.current.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
   };
 
-  const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleMouseEnter = () => {
     setIsMouseEntered(true);
     if (!containerRef.current) return;
   };
 
-  const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleMouseLeave = () => {
     if (!containerRef.current) return;
     setIsMouseEntered(false);
     containerRef.current.style.transform = `rotateY(0deg) rotateX(0deg)`;
@@ -119,7 +119,7 @@ export const CardItem = ({
 
   useEffect(() => {
     handleAnimations();
-  }, [isMouseEntered]);
+  }, [isMouseEntered]); // eslint-disable-line
 
   const handleAnimations = () => {
     if (!ref.current) return;
